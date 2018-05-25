c61bdb93e89859100fac26976bd57d1d1a3dd96d
fuqianzhong <fuqianzhong@meituan.com>
2018-05-25 19:55:27 +0800

    1

---
+++ src/main/java/code/library/A.java
@@ -0,0 +1,15 @@
+package code.library;
+
+/**
+ * @author fuqianzhong
+ * @date 18/5/25
+ */
+public class A {
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+
+    public void f(){
+
+    }
+}
