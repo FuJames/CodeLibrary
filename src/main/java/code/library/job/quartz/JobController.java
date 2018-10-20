1931b1cf66a4cec05c49da7dc80fd61a77faa0cc
fuqianzhong <fuqianzhong@meituan.com>
2018-10-20 14:26:43 +0800

    add log4j2 support;

--- src/main/java/code/library/job/quartz/JobController.java
+++ src/main/java/code/library/job/quartz/JobController.java
@@ -1,6 +1,8 @@
 package code.library.job.quartz;
 
 import org.quartz.SchedulerException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,12 +15,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping(value = "/job")
 public class JobController {
+    private static final Logger logger = LoggerFactory.getLogger(JobController.class);
 
     @Autowired
     private JobService jobService;
 
     @RequestMapping(value="/add")
-    public void addjob(@RequestParam(value="jobClassName")String jobClassName) throws SchedulerException {
+    public void addjob(@RequestParam(value="jobClassName", required = false)String jobClassName) throws SchedulerException {
+        logger.error("fqz ...");
         jobService.addJob(jobClassName);
     }
 
--- src/main/java/code/library/mybatis/Main.java
+++ src/main/java/code/library/mybatis/Main.java
@@ -1,14 +1,25 @@
 package code.library.mybatis;
 
-import code.library.mybatis.useSqlSession.SqlOperation;
+import com.google.common.collect.Lists;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 /**
  * Created by fuqianzhong on 17/9/22.
  */
 public class Main {
-    public static void main(String[] args) {
-        SqlOperation sqlOperation = new SqlOperation();
-        sqlOperation.addUser();
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
+    public static void main(String[] args) {
+//        SqlOperation sqlOperation = new SqlOperation();
+//        sqlOperation.addUser();
+        List<Object> row = Lists.newArrayList();
+        row.add(null);
+        row.add(null);
+        System.out.println(row);
+
+        logger.info("sb");
     }
 }
